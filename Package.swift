b35ae9baf1d3c152ffbe18bbff3936d7e61b3835
dvniqryab <daniil.raby@innowise-group.com>
2023-11-15 17:50:04 +0100

    initial commit

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version: 5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "rwda",
+    dependencies: [
+        .package(url: "https://github.com/johnfairh/CRuby.git", branch: "main")
+    ]
+)
+
---
+++ Sources/main.swift
@@ -0,0 +1,12 @@
+import rwda
+
+ruby_setup()
+rb_require("./rwda/rwda.rb") // Importing Ruby file
+
+var stringArg = rb_str_new_cstr("Swift") // Creating new Ruby string
+var result = rb_funcallv(0, rb_intern("hello"), 1, &(stringArg)) // Calling the function `hello` passing `stringArg`
+var str = rb_string_value_cstr(&(result)) // Extracting the returned value as C string
+
+print(String(cString: str))
+
+ruby_cleanup(0)
